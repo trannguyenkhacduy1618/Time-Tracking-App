74168c7e086f47eefe4de95e820a2726475c5854
trannguyenkhacduy1618 <23050030@student.bdu.edu.vn>
2025-12-19 22:49:54 +0700

    Update connection.py

--- app/database/connection.py
+++ app/database/connection.py
@@ -4,7 +4,7 @@ from app.core.config import settings
 
 # Tạo engine
 engine = create_engine(
-    settings.DATABASE_URL,   # dùng đúng tên biến trong Settings
+    settings.DATABASE_URL,
     pool_pre_ping=True,
     future=True,  # SQLAlchemy 2.0 style
 )
