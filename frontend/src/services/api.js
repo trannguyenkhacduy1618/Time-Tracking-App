77709bd5c87944cd00138880cecd7892f32c8875
trannguyenkhacduy1618 <23050030@student.bdu.edu.vn>
2025-12-19 19:11:40 +0700

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://time-tracking-app-1-uxet.onrender.com",
   timeout: 10000,
 });
 
