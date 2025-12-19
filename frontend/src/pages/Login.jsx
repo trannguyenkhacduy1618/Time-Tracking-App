de34c3fd604e6501b97c0136b48c0ec8d344aeda
trannguyenkhacduy1618 <23050030@student.bdu.edu.vn>
2025-12-19 16:22:04 +0700

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,24 +1,36 @@
 import { useState } from "react";
+import "../styles/login.css";
+import authService from "../services/authService";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log({ username, password });
-        // TODO: call API /auth/login-json
+
+        try {
+            await authService.login(username, password);
+            navigate("/");
+        } catch (err) {
+            alert("Login failed");
+        }
     };
 
     return (
-        <div style={{ maxWidth: 400, margin: "100px auto" }}>
-        <h2>Login</h2>
+        <div className="login-container">
+        <div className="login-card">
+        <h1 className="login-title">Time Tracker</h1>
 
-        <form onSubmit={handleSubmit}>
+        <form className="login-form" onSubmit={handleSubmit}>
         <input
+        type="text"
         placeholder="Username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        required
         />
 
         <input
@@ -26,10 +38,12 @@ export default function Login() {
         placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
+        required
         />
 
         <button type="submit">Login</button>
         </form>
+        </div>
         </div>
     );
 }
