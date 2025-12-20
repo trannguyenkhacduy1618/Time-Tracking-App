e95f136518dc091c3d095ecbb63645433ccb9af7
trannguyenkhacduy1618 <23050030@student.bdu.edu.vn>
2025-12-20 10:28:09 +0700

    Update create_admin.py

--- app/core/create_admin.py
+++ app/core/create_admin.py
@@ -1,4 +1,3 @@
-# create_admin.py
 from app.database.connection import SessionLocal
 from app.database.models import User
 from app.core.security import get_password_hash
@@ -6,24 +5,28 @@ from app.core.security import get_password_hash
 def create_admin():
     db = SessionLocal()
     try:
-        admin_exists = db.query(User).filter(User.username == "admin").first()
-        if admin_exists:
-            print("Admin đã tồn tại!")
-            return
+        admin = db.query(User).filter(User.username == "admin").first()
 
-        admin_user = User(
-            username="admin",
-            email="admin@gmail.com",
-            full_name="Admin",
-            hashed_password=get_password_hash("12345678"),
-            role="admin",
-            is_active=True
-        )
-        db.add(admin_user)
+        if admin:
+            admin.hashed_password = get_password_hash("12345678")
+            admin.role = "admin"
+            admin.is_active = True
+            print("Đã UPDATE admin user!")
+        else:
+            admin = User(
+                username="admin",
+                email="admin@gmail.com",
+                full_name="Admin",
+                hashed_password=get_password_hash("12345678"),
+                role="admin",
+                is_active=True
+            )
+            db.add(admin)
+            print("Đã CREATE admin user!")
+
         db.commit()
-        print("Đã tạo admin user thành công!")
     finally:
         db.close()
 
-if __name__ == "__main__":
+if name == "main":
     create_admin()
